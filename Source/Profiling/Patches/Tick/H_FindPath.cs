2560b4fd89cd6aed46cb5df2d14976f3d7b7622f
Wiri <wiri@gmail.com>
2021-05-29 15:10:29 +1000

    Improve H_FindPath accuracy via moving to the transpiler system.
    If you ever see reports of the pathfinder spiking after pause, check where it is being called from! The pawn tweener (pawn drawing) actually calls some of the pathfinding methods while paused which accumlate and result in a spike. I have no good solution for this, but its an interesting (and un-intended) behavior.

--- Source/Profiling/Patches/Tick/H_FindPath.cs
+++ Source/Profiling/Patches/Tick/H_FindPath.cs
@@ -1,80 +1,29 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using RimWorld.Planet;
 using Verse;
 using Verse.AI;
 
 namespace Analyzer.Profiling
 {
-    [StaticConstructorOnStartup]
+    [Entry("entry.tick.pathfinder", Category.Tick)]
     internal class H_FindPath
     {
         public static bool Active = false;
 
-        public static bool pathing;
-
-        public static int NodeIndex = 0;
-
-        public static Entry p = Entry.Create("entry.tick.pathfinder", Category.Tick, typeof(H_FindPath), false);
-        public static void ProfilePatch()
+        public static IEnumerable<MethodInfo> GetPatchMethods()
         {
-            HarmonyMethod go = new HarmonyMethod(typeof(H_FindPath), nameof(Start));
-            HarmonyMethod biff = new HarmonyMethod(typeof(H_FindPath), nameof(Stop));
-
-            void slop(Type e, string s)
-            {
-                Modbase.Harmony.Patch(AccessTools.Method(e, s), go, biff);
-            }
-
-            MethodInfo mad = AccessTools.Method(typeof(Reachability), nameof(Reachability.CanReach),
-                new[] { typeof(IntVec3), typeof(LocalTargetInfo), typeof(PathEndMode), typeof(TraverseParms) });
-            Modbase.Harmony.Patch(mad, go, biff);
-
-            // slop(typeof(PathFinder), nameof(PathFinder.CalculateDestinationRect));
-            slop(typeof(PathFinder), nameof(PathFinder.GetAllowedArea));
-            slop(typeof(PawnUtility), nameof(PawnUtility.ShouldCollideWithPawns));
-            slop(typeof(PathFinder), nameof(PathFinder.DetermineHeuristicStrength));
-            slop(typeof(PathFinder), nameof(PathFinder.CalculateAndAddDisallowedCorners));
-            slop(typeof(PathFinder), nameof(PathFinder.InitStatusesAndPushStartNode));
-        }
-
-        [HarmonyPriority(Priority.Last)]
-        public static void Start(MethodBase __originalMethod, ref Profiler __state)
-        {
-            if (p.isActive)
-            {
-                __state = p.Start(__originalMethod.Name, __originalMethod);
-            }
-        }
-
-        [HarmonyPriority(Priority.First)]
-        public static void Stop(Profiler __state)
-        {
-            if (p.isActive)
-            {
-                __state?.Stop();
-            }
-        }
-
-        [HarmonyPriority(Priority.First)]
-        public static void Prefix(MethodBase __originalMethod, ref Profiler __state)
-        {
-            if (p.isActive)
-            {
-                __state = p.Start("PathFinder.FindPath", __originalMethod);
-                pathing = true;
-            }
-        }
-
-        [HarmonyPriority(Priority.Last)]
-        public static void Postfix(Profiler __state)
-        {
-            if (p.isActive)
-            {
-                pathing = false;
-                __state?.Stop();
-            }
+            yield return AccessTools.Method(typeof(Reachability), nameof(Reachability.CanReach),
+                new[] {typeof(IntVec3), typeof(LocalTargetInfo), typeof(PathEndMode), typeof(TraverseParms)});
+            yield return AccessTools.Method(typeof(PawnUtility), nameof(PawnUtility.ShouldCollideWithPawns));
+            yield return AccessTools.Method(typeof(PathFinder), nameof(PathFinder.CalculateDestinationRect));
+            yield return AccessTools.Method(typeof(PathFinder), nameof(PathFinder.GetAllowedArea));
+            yield return AccessTools.Method(typeof(PathFinder), nameof(PathFinder.DetermineHeuristicStrength));
+            yield return AccessTools.Method(typeof(PathFinder), nameof(PathFinder.CalculateAndAddDisallowedCorners));
+            yield return AccessTools.Method(typeof(PathFinder), nameof(PathFinder.InitStatusesAndPushStartNode));
         }
     }
 }
